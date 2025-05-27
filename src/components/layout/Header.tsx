03b06dbb43854676ecefaece9871e70cc0c76c5a
Venturesum1 <soumyasisdas8@gmail.com>
2025-05-28 00:35:33 +0530

    changes done

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,9 +8,9 @@ const Header = () => {
   return (
     <div className="relative bg-gradient-to-r from-metro-blue via-metro-green to-metro-purple text-white overflow-hidden bg-[url('/images/bgMetro.jpg')] bg-cover bg-center">
       {/* Background Pattern */}
-      <div className="absolute inset-0 opacity-10">
+      {/* <div className="absolute inset-0 opacity-10">
         <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg%20width%3D%2260%22%20height%3D%2260%22%20viewBox%3D%220%200%2060%2060%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Cg%20fill%3D%22none%22%20fill-rule%3D%22evenodd%22%3E%3Cg%20fill%3D%22%23ffffff%22%20fill-opacity%3D%220.1%22%3E%3Ccircle%20cx%3D%2230%22%20cy%3D%2230%22%20r%3D%222%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
-      </div>
+      </div> */}
       
       <div className="relative max-w-7xl mx-auto px-4 py-12">
         <div className="text-center mb-8">
